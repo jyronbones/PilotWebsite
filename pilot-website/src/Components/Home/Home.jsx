f92426d7fb6e4b8d8b0bb7982864a27eea753c24
Byron <byronjones77@gmail.com>
2023-10-12 14:35:53 -0400

    updated Home to center icons on text and link exterior container

--- pilot-website/src/Components/Home/Home.jsx
+++ pilot-website/src/Components/Home/Home.jsx
@@ -13,52 +13,41 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <div className="Home">
-        <div>
-          <Link to="/Productivity">
-            <FaProductHunt size={50} className="productivity-icon" />
-            <h3 className="productivity-text">Productivity</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/meeting-minutes">
-            <FaClipboard size={50} className="minutes-icon" />
-            <h3 className="minutes-text">Meeting Minutes</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/scheduling">
-            <FaClock size={50} className="scheduling-icon" />
-            <h3 className="scheduling-text">Scheduling</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/klein">
-            <FaBox size={50} className="klein-icon" />
-            <h3 className="klein-text">KLEIN Login</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/union-agreement">
-            <FaHandshake size={50} className="union-icon" />
-            <h3 className="union-text">Union Agreement</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/links">
-            <FaLink size={50} className="links-icon" />
-            <h3 className="links-text">Links</h3>
-          </Link>
-        </div>
-        <div>
-          {/* Placeholder check for admin. When admin functionality is implemented, you can wrap the Link component with a conditional rendering based on the admin status. */}
-          <Link to="/admin-panel">
-            <FaUserShield size={50} className="admin-icon" />
-            <h3 className="admin-text">Admin Panel</h3>
-          </Link>
-        </div>
-      </div>
+    <div className="Home">
+      <Link to="/Productivity" className="link-box">
+        <FaProductHunt size={50} className="icon" />
+        <h3>Productivity</h3>
+      </Link>
+
+      <Link to="/meeting-minutes" className="link-box">
+        <FaClipboard size={50} className="icon" />
+        <h3>Meeting Minutes</h3>
+      </Link>
+
+      <Link to="/scheduling" className="link-box">
+        <FaClock size={50} className="icon" />
+        <h3>Scheduling</h3>
+      </Link>
+
+      <Link to="/klein" className="link-box">
+        <FaBox size={50} className="icon" />
+        <h3>KLEIN Login</h3>
+      </Link>
+
+      <Link to="/union-agreement" className="link-box">
+        <FaHandshake size={50} className="icon" />
+        <h3>Union Agreement</h3>
+      </Link>
+
+      <Link to="/links" className="link-box">
+        <FaLink size={50} className="icon" />
+        <h3>Links</h3>
+      </Link>
+
+      <Link to="/admin-panel" className="link-box">
+        <FaUserShield size={50} className="icon" />
+        <h3>Admin Panel</h3>
+      </Link>
     </div>
   );
 };
