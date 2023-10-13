8898170b20e56e91810dde1f4993664e9151e0c8
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-13 01:32:14 -0400

    Attempt to fix PRB #3

--- pilot-website/src/App.test.js
+++ pilot-website/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import App from "./App";
+
+afterEach(cleanup);
 
 test("renders Login component", () => {
   render(<App />);
-  const loginElement = screen.getByText(/login/i);
+  const loginElement = screen.getByText("Login");
   expect(loginElement).toBeInTheDocument();
 });
