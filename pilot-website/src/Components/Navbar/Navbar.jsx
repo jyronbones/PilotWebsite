36ca7a1f79bf3238892d87ec850c007fb6475b6e
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-13 03:07:07 -0400

    updated NavBar text look and Logout Button look and feel

--- pilot-website/src/Components/Navbar/Navbar.jsx
+++ pilot-website/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import "./Navbar.css";
 
 const CustomNavbar = () => {
   return (
-    // BUG: Fix 'Home' and 'Logout' buttons to be responsive for smaller devices
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="/home">
         <img src="/images/logo.png" alt="Logo" className="navbar-logo" />
@@ -14,13 +13,10 @@ const CustomNavbar = () => {
         </span>
       </Navbar.Brand>
 
-      <Link to="/home" className="nav-link nav-button home-link">
-        Home
-      </Link>
       <Navbar.Collapse>
         <Nav className="ml-auto">
           <Nav.Item>
-            <Link to="/" className="nav-link nav-button navbar-collapse-link">
+            <Link to="/" className="nav-link logout-button">
               Logout
             </Link>
           </Nav.Item>
