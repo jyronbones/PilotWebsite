cd987ab0780577f7af5eba389239ff6ec1bde5c9
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-13 01:01:58 -0400

    updated App test

--- pilot-website/src/App.test.js
+++ pilot-website/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Login component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginElement = screen.getByText(/login/i);
+  expect(loginElement).toBeInTheDocument();
 });
