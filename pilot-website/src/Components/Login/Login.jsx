37de2052ab4c62f5f1032e658f67593fa5ba7589
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-13 20:50:17 -0400

    added linter and formatter automation

--- pilot-website/src/Components/Login/Login.jsx
+++ pilot-website/src/Components/Login/Login.jsx
@@ -1,42 +1,42 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Login.css'
 
 const Login = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const handleLogin = () => {
-    navigate("/home"); // Navigate to the home page
-  };
+    navigate('/home') // Navigate to the home page
+  }
 
   return (
-    <div className="login-container">
-      <h2 className="login-title">Login</h2>
+    <div className='login-container'>
+      <h2 className='login-title'>Login</h2>
       <form
-        className="login-form"
+        className='login-form'
         onSubmit={(e) => {
-          e.preventDefault();
-          handleLogin();
+          e.preventDefault()
+          handleLogin()
         }}
       >
-        <div className="input-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+        <div className='input-group'>
+          <label htmlFor='username'>Username:</label>
+          <input type='text' id='username' name='username' required />
         </div>
 
-        <div className="input-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+        <div className='input-group'>
+          <label htmlFor='password'>Password:</label>
+          <input type='password' id='password' name='password' required />
         </div>
 
-        <div className="actions">
-          <button type="submit" className="login-btn">
+        <div className='actions'>
+          <button type='submit' className='login-btn'>
             Login
           </button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default Login
