8927fecdf609348402affce5c27b885e7040982e
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-13 22:24:20 -0400

    [fix] fixed navbar/home and formatting (#15)
    * fixed navbar/home and formatting
    
    * fix formatting

--- pilot-website/src/Components/Navbar/Navbar.jsx
+++ pilot-website/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,27 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Nav } from "react-bootstrap";
-import "./Navbar.css";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
+import './Navbar.css'
 
 const CustomNavbar = () => {
   return (
-    // BUG: Fix 'Home' and 'Logout' buttons to be responsive for smaller devices
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/home">
-        <img src="/images/logo.png" alt="Logo" className="navbar-logo" />
-        <span className="navbar-brand-text">
-          Upper St. Lawrence Pilots Association
-        </span>
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand href='/home'>
+        <img src='/images/logo.png' alt='Logo' className='navbar-logo' />
+        <span className='navbar-brand-text'>Upper St. Lawrence Pilots Association</span>
       </Navbar.Brand>
 
-      <Link to="/home" className="nav-link nav-button home-link">
-        Home
-      </Link>
       <Navbar.Collapse>
-        <Nav className="ml-auto">
+        <Nav className='ml-auto'>
           <Nav.Item>
-            <Link to="/" className="nav-link nav-button navbar-collapse-link">
+            <Link to='/' className='nav-link logout-button'>
               Logout
             </Link>
           </Nav.Item>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  );
-};
+  )
+}
 
-export default CustomNavbar;
+export default CustomNavbar
