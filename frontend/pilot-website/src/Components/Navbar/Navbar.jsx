e880e1542197c3bf7c51fb8270e25ee9a8975df3
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-19 21:59:39 -0400

    update Features positioning and text

--- frontend/pilot-website/src/Components/Navbar/Navbar.jsx
+++ frontend/pilot-website/src/Components/Navbar/Navbar.jsx
@@ -6,34 +6,39 @@ import './Navbar.css'
 const CustomNavbar = () => {
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='/home'>
-        <img src='/images/logo/logo.png' alt='Logo' className='navbar-logo' />
-        <span className='navbar-brand-text'>Upper St. Lawrence Pilots Association</span>
-      </Navbar.Brand>
+      <div className='navbar-flex-wrapper'>
+        <Navbar.Brand href='/home'>
+          <img src='/images/logo/logo.png' alt='Logo' className='navbar-logo' />
+          <div className='navbar-brand-text'>
+            <span>Upper St. Lawrence</span>
+            <span>Pilots Association</span>
+          </div>
+        </Navbar.Brand>
 
-      <NavDropdown title='Features' id='basic-nav-dropdown'>
-        <NavDropdown.Item as={Link} to='/Productivity'>
-          <h3>Productivity</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/meeting-minutes'>
-          <h3>Meeting Minutes</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/scheduling'>
-          <h3>Scheduling</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/klein'>
-          <h3>KLEIN Login</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/union-agreement'>
-          <h3>Union Agreement</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/links'>
-          <h3>Links</h3>
-        </NavDropdown.Item>
-        <NavDropdown.Item as={Link} to='/admin-panel'>
-          <h3>Admin Panel</h3>
-        </NavDropdown.Item>
-      </NavDropdown>
+        <NavDropdown title='Features' id='basic-nav-dropdown'>
+          <NavDropdown.Item as={Link} to='/Productivity'>
+            <h3>Productivity</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/meeting-minutes'>
+            <h3>Meeting Minutes</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/scheduling'>
+            <h3>Scheduling</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/klein'>
+            <h3>KLEIN Login</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/union-agreement'>
+            <h3>Union Agreement</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/links'>
+            <h3>Links</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/admin-panel'>
+            <h3>Admin Panel</h3>
+          </NavDropdown.Item>
+        </NavDropdown>
+      </div>
 
       <Navbar.Collapse>
         <Nav className='ml-auto'>
