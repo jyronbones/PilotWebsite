a4b3591678dcbd0aebaeababd818e6f94c7ed2e6
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-20 00:41:03 -0400

    Byron navbar/home/schudeling menu update (#23)
    * add Features dropdown menu
    
    * fix dropdown css
    
    * update Features positioning and text
    
    * update features dropdown css
    
    * fix dropdown list to dissapear when links are clicked
    
    * fix formatting
    
    * adjusted comments
    
    * update Home and Scheduling icon tile css
    
    * update comment

--- frontend/pilot-website/src/Components/Navbar/Navbar.jsx
+++ frontend/pilot-website/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import './Navbar.css'
 
 const CustomNavbar = () => {
+  const dropdownRef = useRef(null)
+
+  // BUG: Needs a fix to close dropdown by clicking anything outside the dropmenu and also links(working)
+  const closeDropdown = () => {
+    if (dropdownRef.current) {
+      dropdownRef.current.toggle()
+    }
+  }
+
   return (
     <Navbar bg='light' expand='lg'>
-      <Navbar.Brand href='/home'>
-        <img src='/images/logo/logo.png' alt='Logo' className='navbar-logo' />
-        <span className='navbar-brand-text'>Upper St. Lawrence Pilots Association</span>
-      </Navbar.Brand>
+      <div className='navbar-flex-wrapper'>
+        <Navbar.Brand href='/home'>
+          <img src='/images/logo/logo.png' alt='Logo' className='navbar-logo' />
+          <div className='navbar-brand-text'>
+            <span>Upper St. Lawrence</span>
+            <span>Pilots Association</span>
+          </div>
+        </Navbar.Brand>
+
+        <NavDropdown title='Features' id='basic-nav-dropdown' ref={dropdownRef}>
+          <NavDropdown.Item as={Link} to='/Productivity' onClick={closeDropdown}>
+            <h3>Productivity</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/meeting-minutes' onClick={closeDropdown}>
+            <h3>Meeting Minutes</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/scheduling' onClick={closeDropdown}>
+            <h3>Scheduling</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/klein' onClick={closeDropdown}>
+            <h3>KLEIN Login</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/union-agreement' onClick={closeDropdown}>
+            <h3>Union Agreement</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/links' onClick={closeDropdown}>
+            <h3>Links</h3>
+          </NavDropdown.Item>
+          <NavDropdown.Item as={Link} to='/admin-panel' onClick={closeDropdown}>
+            <h3>Admin Panel</h3>
+          </NavDropdown.Item>
+        </NavDropdown>
+      </div>
 
       <Navbar.Collapse>
         <Nav className='ml-auto'>
