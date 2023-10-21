cc859fcbbf40bab2f7f6078d2a2ada4d9fc29365
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-20 21:37:14 -0400

    Byron remove dropdown (#24)
    * add Features dropdown menu
    
    * fix dropdown css
    
    * update Features positioning and text
    
    * update features dropdown css
    
    * fix dropdown list to dissapear when links are clicked
    
    * fix formatting
    
    * adjusted comments
    
    * update Home and Scheduling icon tile css
    
    * update comment
    
    * remove dropdown menu and styling

--- frontend/pilot-website/src/Components/Navbar/Navbar.jsx
+++ frontend/pilot-website/src/Components/Navbar/Navbar.jsx
@@ -23,30 +23,6 @@ const CustomNavbar = () => {
             <span>Pilots Association</span>
           </div>
         </Navbar.Brand>
-
-        <NavDropdown title='Features' id='basic-nav-dropdown' ref={dropdownRef}>
-          <NavDropdown.Item as={Link} to='/Productivity' onClick={closeDropdown}>
-            <h3>Productivity</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/meeting-minutes' onClick={closeDropdown}>
-            <h3>Meeting Minutes</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/scheduling' onClick={closeDropdown}>
-            <h3>Scheduling</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/klein' onClick={closeDropdown}>
-            <h3>KLEIN Login</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/union-agreement' onClick={closeDropdown}>
-            <h3>Union Agreement</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/links' onClick={closeDropdown}>
-            <h3>Links</h3>
-          </NavDropdown.Item>
-          <NavDropdown.Item as={Link} to='/admin-panel' onClick={closeDropdown}>
-            <h3>Admin Panel</h3>
-          </NavDropdown.Item>
-        </NavDropdown>
       </div>
 
       <Navbar.Collapse>
