2be5b3783b082e188e15fd62b92d1aa6210b5776
Byron <80455013+jyronbones@users.noreply.github.com>
2023-10-20 21:50:19 -0400

    remove dropdown remnants

--- frontend/pilot-website/src/Components/Navbar/Navbar.jsx
+++ frontend/pilot-website/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,16 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import './Navbar.css'
 
 const CustomNavbar = () => {
-  const dropdownRef = useRef(null)
-
-  // BUG: Needs a fix to close dropdown by clicking anything outside the dropmenu and also links(working)
-  const closeDropdown = () => {
-    if (dropdownRef.current) {
-      dropdownRef.current.toggle()
-    }
-  }
-
   return (
     <Navbar bg='light' expand='lg'>
       <div className='navbar-flex-wrapper'>
         <Navbar.Brand href='/home'>
           <img src='/images/logo/logo.png' alt='Logo' className='navbar-logo' />
           <div className='navbar-brand-text'>
-            <span>Upper St. Lawrence</span>
-            <span>Pilots Association</span>
+            <span>Upper St. Lawrence Pilots Association</span>
           </div>
         </Navbar.Brand>
       </div>
