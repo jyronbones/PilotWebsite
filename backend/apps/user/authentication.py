0878052ac04f49e01e5b4e4cef1b6104c87d5c22
Byron <80455013+jyronbones@users.noreply.github.com>
2024-02-07 19:04:21 -0500

    DynamoDB implementation (#48)
    * dynamodb integration
    
    * minor changes

---
+++ backend/apps/user/authentication.py
@@ -0,0 +1,30 @@
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from .models import outstandingToken, UserNew  # Assuming Token is your DynamoDB model for storing tokens
+import jwt
+from django.conf import settings
+
+
+class DynamoDBJWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return None
+        # Extract JWT token from the Authorization header
+        if token.startswith('Bearer '):
+            token = token.split(' ')[1]  # Remove 'Bearer ' prefix
+        try:
+            # Decode the token
+            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+
+            # Check if token exists in DynamoDB
+            token_record = list(outstandingToken.scan(jti=decoded_token['jti']))
+
+            user_record = list(UserNew.scan(id=decoded_token["user_id"]))
+
+            # You may need to add additional checks here, like token expiration
+
+            return user_record[0], token_record[0]  # Return the authenticated token and user record
+        except outstandingToken.DoesNotExist:
+            raise AuthenticationFailed('Invalid token')
