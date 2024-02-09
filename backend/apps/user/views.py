c5fd23949a3bb533c0d86ef689201cd7749627ce
Eri Le <le000222@algonquinlive.com>
2024-02-08 22:01:08 -0500

    Uncomment urls.py and views.py

--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -234,69 +234,69 @@ def auth_me(request):
 
 # # This view is just for testing purpose,
 # # if you want to create an admin quickly without any restrictions you can use this
-# @api_view(["POST"])
-# def create_admin_account(request):
-#     try:
-#         user_id = str(uuid.uuid4())
-#         full_name = request.data["full_name"]
-#         first_name = ""
-#         last_name = ""
-#         email = request.data["email"]
-#         user_type = int(request.data["user_type"])
-#         password = make_password(request.data["password"])
-#         date_joined = timestamp
-#         created_at = timestamp
-#         updated_at = timestamp
-#         is_superuser = False
-#         is_staff = False
-#         is_active = True
-#         is_authenticated = True
-#         last_login = None
-#
-#         # If the new user is admin then do this:
-#         if user_type == 1:
-#             is_superuser = True
-#             is_staff = True
-#
-#         # Retrun a fail msg if the user is already exist in the DB
-#         user_exist = check_user(email)
-#         if user_exist:
-#             return Response(
-#                 {
-#                     "success": False,
-#                     "message": f"User with the {email} email is already exist!",
-#                 }
-#             )
-#
-#         # New user data to be saved on database:
-#         record = UserNew(
-#             id=user_id,
-#             full_name=full_name,
-#             first_name=first_name,
-#             last_name=last_name,
-#             email=email,
-#             user_type=user_type,
-#             password=password,
-#             date_joined=date_joined,
-#             created_at=created_at,
-#             updated_at=updated_at,
-#             is_superuser=is_superuser,
-#             is_staff=is_staff,
-#             is_active=is_active,
-#             is_authenticated=is_authenticated,
-#             last_login=last_login,
-#         )
-#         # Save the data gathered for new user on DynamoDB
-#         record.save()
-#         return Response(
-#             {"success": True, "message": "User created successfully"},
-#             status.HTTP_201_CREATED,
-#         )
-#     except Exception as e:
-#         return Response(
-#             {"success": False, "message": f"Bad request: {str(e)}"},
-#             status.HTTP_400_BAD_REQUEST,
-#         )
+@api_view(["POST"])
+def create_admin_account(request):
+    try:
+        user_id = str(uuid.uuid4())
+        full_name = request.data["full_name"]
+        first_name = ""
+        last_name = ""
+        email = request.data["email"]
+        user_type = int(request.data["user_type"])
+        password = make_password(request.data["password"])
+        date_joined = timestamp
+        created_at = timestamp
+        updated_at = timestamp
+        is_superuser = False
+        is_staff = False
+        is_active = True
+        is_authenticated = True
+        last_login = None
+
+        # If the new user is admin then do this:
+        if user_type == 1:
+            is_superuser = True
+            is_staff = True
+
+        # Retrun a fail msg if the user is already exist in the DB
+        user_exist = check_user(email)
+        if user_exist:
+            return Response(
+                {
+                    "success": False,
+                    "message": f"User with the {email} email is already exist!",
+                }
+            )
+
+        # New user data to be saved on database:
+        record = UserNew(
+            id=user_id,
+            full_name=full_name,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            user_type=user_type,
+            password=password,
+            date_joined=date_joined,
+            created_at=created_at,
+            updated_at=updated_at,
+            is_superuser=is_superuser,
+            is_staff=is_staff,
+            is_active=is_active,
+            is_authenticated=is_authenticated,
+            last_login=last_login,
+        )
+        # Save the data gathered for new user on DynamoDB
+        record.save()
+        return Response(
+            {"success": True, "message": "User created successfully"},
+            status.HTTP_201_CREATED,
+        )
+    except Exception as e:
+        return Response(
+            {"success": False, "message": f"Bad request: {str(e)}"},
+            status.HTTP_400_BAD_REQUEST,
+        )
 
 
 @api_view(["POST", "PUT", "GET", "DELETE"])
