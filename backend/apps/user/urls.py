bb757e182b036c31fa15b62348f756ed646b7826
Garrison <mikeagarrison@gmail.com>
2024-03-15 23:18:29 -0400

    Updates to scheduling 005

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -4,7 +4,7 @@ from .views import (
     auth_me,
     refresh_token,
     admin_user_crud,
-    # create_admin_account,
+    create_admin_account,
     test_aws,
 )
 
@@ -13,7 +13,7 @@ urlpatterns = [
     re_path(r"^refresh-token", refresh_token),
     re_path(r"^auth-me", auth_me),
     re_path(r"^user", admin_user_crud),
-    # re_path(r"^create_admin", create_admin_account),
+    re_path(r"^create_admin", create_admin_account),
     re_path(r"^test_aws", test_aws),
 ]
 
