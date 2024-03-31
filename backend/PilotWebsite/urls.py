999dbadba51554830a771350ed7c49c476bcac0c
Garrison <mikeagarrison@gmail.com>
2024-03-31 14:35:50 -0400

    Changes to apps/files and Urls.py

--- backend/PilotWebsite/urls.py
+++ backend/PilotWebsite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.user.urls')),
-    path('api/v1/', include('apps.files.urls')),
+    path('api/v1/', include('apps.agreementfiles.urls')),
+    path('api/v1/', include('apps.meetingminutes.urls')),
     path('api/v1/scheduling/', include('apps.scheduling.urls')),
 ]
